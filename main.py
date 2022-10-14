fc4d7bd7f927e0329d403162660eea49d6ebce81
Bruno <57503219+Hinje@users.noreply.github.com>
2022-10-14 09:49:50 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-# This is the main file
+#coding 😎
+
+
+import csv
+
 
 def black_jack():
     ...
+
+
+def load_strategy_table():
+    my_file = "Strategy.csv"
+    my_data = []
+    with open(my_file, newline='') as csvfile:
+        my_reader = csv.reader(csvfile, delimiter=",")
+        for row in my_reader:
+            my_data.append(row)
+        return (my_data)
+
+
+if __name__ == "__main__":
+    hand_data = load_strategy_table()
+
 
