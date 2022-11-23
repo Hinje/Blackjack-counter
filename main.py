e1fabcaca3c4190cf51791e3b450a2a699886a0c
Bruno <57503219+Hinje@users.noreply.github.com>
2022-11-23 09:01:41 +0000

    Update main.py
    Added code from home

--- main.py
+++ main.py
@@ -4,8 +4,52 @@
 import csv
 
 
-def black_jack():
-    ...
+class Card:
+    def __init__(self, name, suit, value):
+        self.name = name
+        self.suit = suit
+        self.value = value
+ 
+    def showcard(self):
+        ...
+        
+        
+
+
+
+class Deck_of_Cards:
+    def __init__(self):
+        self.card_list = []
+        self.remake()
+ 
+    def create_deck(self):
+        self.cards_list.clear()
+        names = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+        suits = ["♣", "♠", "♦", "♥"]
+           
+    #    for i in range(len(names)):
+           # for suit in suits:
+           #     self.card_list.append
+                
+                
+ class Bot:
+    def __init__(self):
+        self.hand = []
+        self.chips = 10000
+        
+        
+    def print_hand(self):
+        number_of_cards = len(self.hand)
+
+
+    def calculate_hand(self):
+           value = 0
+           for card in self.hand:
+               value += card.value
+
+
+
+
 
 
 def load_strategy_table():
