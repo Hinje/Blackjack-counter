c94ae457b24689920d416a47d73ebf8575dd3f6f
bruvar0809 <bruvar0809@highgateschool.org.uk>
2022-11-23 13:01:16 +0000

    Made a CardSet Class and BlackJackDeck as a subclass. Created a value routine for the CardSet Added a couple of tests for CardSet

--- main.py
+++ main.py
@@ -1,55 +1,68 @@
-#coding 😎
+# coding 😎
 
 
 import csv
 
 
 class Card:
-    def __init__(self, name, suit, value):
+    values = {"A": 11, "K": 10, "Q": 10, "J": 10, "T": 10, "9": 9,
+             "8": 8, "7": 7, "6": 6, "5": 5, "4": 4, "3": 3, "2": 2}
+
+    def __init__(self, name, suit):
         self.name = name
         self.suit = suit
-        self.value = value
- 
+        self.value = self.values[name]
+
     def showcard(self):
         ...
-        
-        
+
+    def __str__(self):
+        return f'{self.suit}{self.name}'
 
 
+class CardSet:
+    names = list("AKQJT98765432")
+    suits = ["♣", "♠", "♦", "♥"]
 
-class Deck_of_Cards:
+    def __init__(self):
+        self.cards = []
+
+    def add_card(self, card):
+        self.cards.append(card)
+
+    def calc_value(self):
+        # TODO must account for Aces
+        return sum(card.value for card in self.cards)
+
+
+class BlackJackDeck(CardSet):
+
     def __init__(self):
         self.card_list = []
-        self.remake()
- 
+        self.create_deck()
+
     def create_deck(self):
-        self.cards_list.clear()
-        names = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
-        suits = ["♣", "♠", "♦", "♥"]
-           
-    #    for i in range(len(names)):
-           # for suit in suits:
-           #     self.card_list.append
-                
-                
- class Bot:
+        self.card_list = [Card(nm, st)
+                          for nm in self.names
+                          for st in self.suits]
+
+    # for i in range(len(names)):
+    # for suit in suits:
+    # self.card_list.append
+
+
+class Bot:
     def __init__(self):
         self.hand = []
         self.chips = 10000
-        
-        
+
     def print_hand(self):
         number_of_cards = len(self.hand)
 
-
     def calculate_hand(self):
-           value = 0
-           for card in self.hand:
-               value += card.value
-
-
-
-
+        value = 0
+        for card in self.hand:
+            value += card.value
 
 
 def load_strategy_table():
@@ -65,5 +78,3 @@ def load_strategy_table():
 if __name__ == "__main__":
     hand_data = load_strategy_table()
     print(hand_data[0])
-
-
--- test_main.py
+++ test_main.py
@@ -1,6 +1,21 @@
-from main import load_strategy_table
+from main import load_strategy_table, Card, CardSet
 
 
 def test_load_strategy_table():
     hand_data = load_strategy_table()
     assert hand_data[0] == ['H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H', 'H']
+
+
+def test_add_card():
+    my_card_set = CardSet()
+    my_card_set.add_card(Card('A', "♦"))
+    my_card_set.add_card(Card('4', "♠"))
+    assert my_card_set.cards[1].suit == "♠"
+    assert my_card_set.cards[1].name == "4"
+
+
+def test_calc_value():
+    my_card_set = CardSet()
+    my_card_set.add_card(Card('A', "♦"))
+    my_card_set.add_card(Card('4', "♠"))
+    assert my_card_set.calc_value() == 15
